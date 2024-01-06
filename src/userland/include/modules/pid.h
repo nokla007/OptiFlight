8fad8049ec93d44e4219e2c80190b6cdd6770ec9
nokla007 <javazawad@gmail.com>
2024-01-06 13:29:23 +0600

    pid initial

--- src/userland/include/modules/pid.h
+++ src/userland/include/modules/pid.h
@@ -2,7 +2,9 @@
 #ifndef __PID_H
 #define __PID_H
 #include <stdint.h>
+#include <math.h>
 
+double PID_Controller(double set_value, double mea_value, double max_value, double min_value, double Kp, double Ki, double Kd, double Ts);
 
 
 #endif
--- src/userland/modules/control/pid.c
+++ src/userland/modules/control/pid.c
@@ -1,5 +1,27 @@
 #include <modules/pid.h>
 
-int demo_pid() {
-    return 0;
+double PID_Controller(double set_value, double mea_value, double max_value, double min_value, double Kp, double Ki, double Kd, double Ts)
+{
+    double output, error;
+    static double pre_error = 0;    // previous error
+    static double integral = 0;
+    double derivative;
+
+    error = set_value - mea_value;
+    integral = integral + error * Ts;
+    derivative = (error - pre_error) / Ts;
+    output = Kp * error + Ki * integral + Kd * derivative;
+
+    if (output > max_value)
+    {
+        output = max_value;
+    }
+    else if (output < min_value)
+    {
+        output = min_value;
+    }
+
+    pre_error = error;
+
+    return output;
 }
